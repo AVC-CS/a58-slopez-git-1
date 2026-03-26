e4bc057babd8945360eda326c958511ab89c4ac0
Kyu Woong Lee <kwooler@gmail.com>
2026-03-26 12:54:36 -0700

    Student starter code main_write.cpp

--- main_write.cpp
+++ main_write.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+    int N;
+    double score1, score2;
+    string stuName;
+    ofstream ofs;
+
+    // TODO: open "students.txt" for writing
+
+    // TODO: check if file opened successfully; print error and exit if not
+
+    cout << "Enter the total number of students:";
+    // TODO: read N from cin, write N to ofs
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Enter the student name and two scores: ";
+        // TODO: read stuName, score1, score2 from cin; write them to ofs
+    }
+    // TODO: close ofs
+    return 0;
 }
